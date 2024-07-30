d04a408870fa9e0332a883930c0b8c830fc5af90
highlaw00 <choiyool00@gmail.com>
2024-07-30 12:22:23 +0900

    feat: 회원 소갯말 필드 추가

--- init-scripts/init.sql
+++ init-scripts/init.sql
@@ -7,7 +7,8 @@ DROP TABLE if EXISTS comment;
 CREATE TABLE member (
                         member_id BIGINT AUTO_INCREMENT PRIMARY KEY,
                         username VARCHAR(50) NOT NULL,
-                        password VARCHAR(255) NOT NULL
+                        password VARCHAR(255) NOT NULL,
+                        intro VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE article (
