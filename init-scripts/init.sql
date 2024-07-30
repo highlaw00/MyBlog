f70f6151658d5a1c182352e30d0af95b0b0cac1d
highlaw00 <choiyool00@gmail.com>
2024-07-30 16:49:30 +0900

    docs: 테스트 데이터 추가 및 member.intro 필드 추가

--- init-scripts/init.sql
+++ init-scripts/init.sql
@@ -8,7 +8,7 @@ CREATE TABLE member (
                         member_id BIGINT AUTO_INCREMENT PRIMARY KEY,
                         username VARCHAR(50) NOT NULL,
                         password VARCHAR(255) NOT NULL,
-                        intro VARCHAR(255) NOT NULL
+                        intro VARCHAR(255)
 );
 
 CREATE TABLE article (
@@ -27,3 +27,7 @@ CREATE TABLE comment (
                          CONSTRAINT comment_member_fk FOREIGN KEY(member_id) REFERENCES member(member_id) ON DELETE CASCADE,
                          CONSTRAINT comment_article_fk FOREIGN KEY (article_id) REFERENCES article(article_id) ON DELETE CASCADE
 );
+
+INSERT INTO member(username, password, intro) VALUES('test1', '12345', '안녕하세요. 테스트1입니다.');
+INSERT INTO member(username, password, intro) VALUES('test2', '12345', '안녕하세요. 테스트2입니다.');
+INSERT INTO member(username, password, intro) VALUES('test3', '12345', '안녕하세요. 테스트3입니다.');
