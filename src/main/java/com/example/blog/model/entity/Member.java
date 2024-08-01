508cb4432ca86557ea295c98d1e68a8e27207557
highlaw00 <choiyool00@gmail.com>
2024-08-01 18:11:16 +0900

    chore: 엔티티, Dto, Mapper 패키지 구분에 따른 패키지명 변경

---
+++ src/main/java/com/example/blog/model/entity/Member.java
@@ -0,0 +1,19 @@
+package com.example.blog.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "member")
+@Getter
+@Setter
+public class Member {
+    @Id
+    @Column(name = "member_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String username;
+    String password;
+    String intro;
+}
