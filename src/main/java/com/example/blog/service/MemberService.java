57a574a30ae5668075552e95155a1fdf191eefd8
highlaw00 <choiyool00@gmail.com>
2024-08-01 19:13:12 +0900

    feat: 회원 중복 확인, 회원 등록 기능 구현

--- src/main/java/com/example/blog/service/MemberService.java
+++ src/main/java/com/example/blog/service/MemberService.java
@@ -1,8 +1,10 @@
 package com.example.blog.service;
 
+import com.example.blog.exception.MemberDuplicatedException;
 import com.example.blog.model.dto.MemberDto;
 import com.example.blog.exception.MemberNotFoundException;
 import com.example.blog.model.entity.Member;
+import com.example.blog.model.mapper.MemberMapper;
 import com.example.blog.repository.MemberRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +16,13 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    public void checkDuplication(MemberDto dto) {
+        Member member = memberRepository.findByUsername(dto.getUsername());
+        if (member != null) {
+            throw new MemberDuplicatedException();
+        }
+    }
+
     @Transactional
     public Member updateMember(Long id, MemberDto memberDto) {
         Member member = memberRepository.findById(id).orElseThrow(MemberNotFoundException::new);
@@ -22,6 +31,12 @@ public class MemberService {
         member.setPassword(memberDto.getPassword());
         member.setIntro(memberDto.getIntro());
 
+        return member;
+    }
+
+    public Member postMember(MemberDto dto) {
+        Member member = MemberMapper.toEntity(dto);
+        member = memberRepository.save(member);
         return member;
     }
 }
