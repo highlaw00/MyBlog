e1bdb138dab2ed845f9d1a04041cbec567614e27
highlaw00 <choiyool00@gmail.com>
2024-08-05 14:22:06 +0900

    feat: 게시물 예외 핸들러 추가

---
+++ src/main/java/com/example/blog/handler/ArticleExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.example.blog.handler;
+
+import com.example.blog.exception.ArticleNotFoundException;
+import com.example.blog.model.dto.ArticleDto;
+import com.example.blog.utils.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ArticleExceptionHandler {
+    @ExceptionHandler
+    public ApiResponse<ArticleDto> handleArticleNotFound(ArticleNotFoundException e) {
+        return ApiResponse.createFailureResponse(null, HttpStatus.NOT_FOUND, e.getMessage());
+    }
+}
