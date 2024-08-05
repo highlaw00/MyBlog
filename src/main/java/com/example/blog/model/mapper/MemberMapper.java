efeccddd214168988ac4067c0dcb2df86f107b19
highlaw00 <choiyool00@gmail.com>
2024-08-05 14:19:50 +0900

    feat: null check 추가

--- src/main/java/com/example/blog/model/mapper/MemberMapper.java
+++ src/main/java/com/example/blog/model/mapper/MemberMapper.java
@@ -9,28 +9,39 @@ import java.util.stream.Stream;
 
 public class MemberMapper {
     public static Member toEntity(MemberDto dto) {
-        Member member = new Member();
 
-        member.setId(dto.getId());
-        member.setUsername(dto.getUsername());
-        member.setPassword(dto.getPassword());
-        member.setIntro(dto.getIntro());
+        if (dto == null) {
+            return null;
+        }
 
-        return member;
+        return new Member(
+                dto.getId(),
+                dto.getUsername(),
+                dto.getPassword(),
+                dto.getIntro()
+        );
     }
 
     public static MemberDto toDto(Member member) {
-        MemberDto dto = new MemberDto();
 
-        dto.setId(member.getId());
-        dto.setUsername(member.getUsername());
-        dto.setPassword(member.getPassword());
-        dto.setIntro(member.getIntro());
+        if (member == null) {
+            return null;
+        }
 
-        return dto;
+        return new MemberDto(
+                member.getId(),
+                member.getUsername(),
+                member.getPassword(),
+                member.getIntro()
+        );
+
     }
 
     public static List<MemberDto> toDtos(List<Member> members) {
+        if (members == null) {
+            return null;
+        }
+
         return members.stream().map(MemberMapper::toDto).toList();
     }
 }
