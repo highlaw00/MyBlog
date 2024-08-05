0dad0e169deb2eb60dd6f5fc9ca6caf0ac9bf1fe
highlaw00 <choiyool00@gmail.com>
2024-08-05 14:18:12 +0900

    feat: 생성자 추가

--- src/main/java/com/example/blog/model/dto/MemberDto.java
+++ src/main/java/com/example/blog/model/dto/MemberDto.java
@@ -4,9 +4,13 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MemberDto {
 
     private Long id;
