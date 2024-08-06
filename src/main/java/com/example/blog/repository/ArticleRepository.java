874ef513535361763274fbed3a9bd50cdea8d942
highlaw00 <choiyool00@gmail.com>
2024-08-06 21:17:06 +0900

    feat: 회원에 종속적인 게시물 조회 쿼리 메서드 구현

--- src/main/java/com/example/blog/repository/ArticleRepository.java
+++ src/main/java/com/example/blog/repository/ArticleRepository.java
@@ -2,6 +2,14 @@ package com.example.blog.repository;
 
 import com.example.blog.model.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+    @Query("select a from Article a inner join a.member m where m.id=:memberId")
+    List<Article> findArticlesByMemberId(@Param("memberId") Long memberId);
+
 }
