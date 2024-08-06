0fbbaebe9edea94b538944e47fd10821260ee9f7
highlaw00 <choiyool00@gmail.com>
2024-08-06 21:19:17 +0900

    feat: 게시물 CRUD API에 필요한 서비스 로직 구현

--- src/main/java/com/example/blog/service/ArticleService.java
+++ src/main/java/com/example/blog/service/ArticleService.java
@@ -2,6 +2,7 @@ package com.example.blog.service;
 
 import com.example.blog.exception.ArticleNotFoundException;
 import com.example.blog.model.dto.ArticleDto;
+import com.example.blog.model.dto.ArticleResponseDto;
 import com.example.blog.model.dto.MemberDto;
 import com.example.blog.model.entity.Article;
 import com.example.blog.model.mapper.ArticleMapper;
@@ -21,20 +22,49 @@ public class ArticleService {
     private final ArticleRepository articleRepository;
     private final MemberService memberService;
 
-    public List<ArticleDto> findAll() {
+    public ArticleResponseDto delete(Long id) {
+        Article article = this.find(id);
+        articleRepository.delete(article);
+        return ArticleMapper.toResponseDto(article);
+    }
+
+    public List<ArticleResponseDto> findAll() {
         List<Article> articles = articleRepository.findAll();
-        return articles.stream().map(ArticleMapper::toDto).toList();
+        return articles.stream().map(ArticleMapper::toResponseDto).toList();
     }
 
-    public ArticleDto findById(Long id) {
-        Optional<Article> articleOptional = articleRepository.findById(id);
-        return ArticleMapper.toDto(articleOptional.orElseThrow(ArticleNotFoundException::new));
+    public List<ArticleResponseDto> findAll(Long memberId) {
+        List<Article> articles = articleRepository.findArticlesByMemberId(memberId);
+        return articles.stream().map(ArticleMapper::toResponseDto).toList();
     }
 
-    public ArticleDto post(ArticleDto dto) {
-        MemberDto memberDto = memberService.findById(dto.getMemberId());
+    public ArticleResponseDto findById(Long id) {
+        Article article = articleRepository.findById(id).orElseThrow(ArticleNotFoundException::new);
+        return ArticleMapper.toResponseDto(article);
+    }
+
+    /**
+     * 수정하려는 Article Entity의 member는 변경되지 않습니다.
+     */
+    public ArticleResponseDto update(ArticleDto dto) {
+        Article article = this.find(dto.getId());
+        article.updateTitleAndContents(dto);
+        articleRepository.saveAndFlush(article);
+        return ArticleMapper.toResponseDto(article);
+    }
+
+    public ArticleResponseDto post(ArticleDto dto) {
+        MemberDto memberDto = getMemberDto(dto);
         Article article = ArticleMapper.toEntity(dto, memberDto);
-        articleRepository.save(article);
-        return ArticleMapper.toDto(article);
+        article = articleRepository.save(article);
+        return ArticleMapper.toResponseDto(article);
+    }
+
+    private MemberDto getMemberDto(ArticleDto dto) {
+        return memberService.findById(dto.getMemberId());
+    }
+
+    private Article find(Long id) {
+        return articleRepository.findById(id).orElseThrow(ArticleNotFoundException::new);
     }
 }
