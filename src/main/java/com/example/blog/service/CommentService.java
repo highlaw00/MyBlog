08d1f52c57269cefd382ac33cb421675f2820423
Highlaw <choiyool00@gmail.com>
2024-08-07 16:55:46 +0900

    feat: CRUD 로직 구현

--- src/main/java/com/example/blog/service/CommentService.java
+++ src/main/java/com/example/blog/service/CommentService.java
@@ -1,12 +1,14 @@
 package com.example.blog.service;
 
 import com.example.blog.exception.CommentNotFoundException;
-import com.example.blog.model.dto.CommentDto;
+import com.example.blog.model.dto.CommentPatchRequestDto;
+import com.example.blog.model.dto.CommentPostRequestDto;
 import com.example.blog.model.dto.CommentResponseDto;
+import com.example.blog.model.entity.Article;
 import com.example.blog.model.entity.Comment;
+import com.example.blog.model.entity.Member;
 import com.example.blog.model.mapper.CommentMapper;
 import com.example.blog.repository.CommentRepository;
-import com.example.blog.utils.ApiResponse;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,6 +21,14 @@ import java.util.List;
 public class CommentService {
 
     private final CommentRepository commentRepository;
+    private final ArticleService articleService;
+    private final MemberService memberService;
+
+    public int delete(Long id) {
+        Comment comment = this.find(id);
+        commentRepository.delete(comment); // Exception 처리 고려
+        return 1;
+    }
 
     public CommentResponseDto findById(Long id) {
         Comment comment = commentRepository.findById(id).orElseThrow(CommentNotFoundException::new);
@@ -33,5 +43,29 @@ public class CommentService {
     public List<CommentResponseDto> findAllOnMember(Long memberId) {
         List<Comment> comments = commentRepository.findCommentsByMemberId(memberId);
         return comments.stream().map(CommentMapper::toResponseDto).toList();
+    }
+
+    public CommentResponseDto post(CommentPostRequestDto dto) {
+        Comment comment = CommentMapper.toEntity(dto);
+        Article article = articleService.findById(dto.getArticleId());
+        Member member = memberService.findById(dto.getMemberId());
+
+        comment.setMember(member);
+        comment.setArticle(article);
+
+        commentRepository.saveAndFlush(comment);
+
+        return CommentMapper.toResponseDto(comment);
+    }
+
+    public CommentResponseDto update(Long id, CommentPatchRequestDto dto) {
+        Comment comment = this.find(id);
+        comment.updateContents(dto);
+        commentRepository.saveAndFlush(comment);
+        return CommentMapper.toResponseDto(comment); // 이 과정에서 Member, Article에 접근해서 추가 쿼리가 발생.
+    }
+
+    private Comment find(Long id) {
+        return commentRepository.findById(id).orElseThrow(CommentNotFoundException::new);
     }
 }
