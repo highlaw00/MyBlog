d79fe4eb151987149672a253f6a1cdb08fc0d9f8
Highlaw <choiyool00@gmail.com>
2024-08-07 16:56:27 +0900

    feat: 페치 조인 적용

--- src/main/java/com/example/blog/repository/CommentRepository.java
+++ src/main/java/com/example/blog/repository/CommentRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    @Query("select c from Comment c join c.article a where a.id=:articleId")
+    @Query("select c from Comment c join fetch c.article a where a.id=:articleId")
     List<Comment> findCommentsByArticleId(@Param("articleId") Long articleId);
 
     @Query("select c from Comment c join c.member m where m.id=:memberId")
