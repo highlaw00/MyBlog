ea1460f126417f5a020790f814093a8ff7c2720f
Highlaw <choiyool00@gmail.com>
2024-08-07 16:52:36 +0900

    feat: Post DTO, Patch DTO 분리

---
+++ src/main/java/com/example/blog/model/dto/CommentPatchRequestDto.java
@@ -0,0 +1,17 @@
+package com.example.blog.model.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CommentPatchRequestDto {
+    @NotNull
+    Long memberId;
+
+    @NotNull
+    @NotBlank
+    String contents;
+}
