be4c110517a0c2e15bfbdeb24cf36b65d1b5174c
highlaw00 <choiyool00@gmail.com>
2024-08-13 22:38:38 +0900

    feat: JWT 필터 설정 및 PasswordEncoder 변경
    - PasswordEncoder 구현체 변경
    - JWT 필터 FilterChain에 등록
    - 따로 세션을 저장하지 않기 때문에 세션 전략 변경

--- src/main/java/com/example/blog/security/SecurityConfig.java
+++ src/main/java/com/example/blog/security/SecurityConfig.java
@@ -1,24 +1,20 @@
 package com.example.blog.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.blog.utils.jwt.JWTUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.logout.LogoutFilter;
-import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -28,14 +24,20 @@ import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity(debug = true)
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Autowired
-    private UserDetailsService userDetailsService;
+    private final AuthenticationConfiguration authenticationConfiguration; // Question: what is authConfiguration?
+    private final JWTUtil jwtUtil;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
@@ -53,50 +55,28 @@ public class SecurityConfig {
         return source;
     }
 
-//    @Bean
-//    public AuthenticationProvider authenticationProvider() {
-//        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-//        provider.setPasswordEncoder(this.passwordEncoder());
-//        provider.setUserDetailsService(this.userDetailsService);
-//        return provider;
-//    }
-//
-//    @Bean
-//    public AuthenticationManager authenticationManager() throws Exception {
-//        return new ProviderManager(this.authenticationProvider());
-//    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(
-            UserDetailsService userDetailsService,
-            PasswordEncoder passwordEncoder) {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsService);
-        authenticationProvider.setPasswordEncoder(passwordEncoder);
-
-        return new ProviderManager(authenticationProvider);
-    }
-
-    public UsernamePasswordAuthenticationFilter authenticationFilter(
-            AuthenticationManager authenticationManager
-    ) throws Exception {
-        UsernamePasswordAuthenticationFilter filter = new JsonAuthenticationFilter(authenticationManager);
-        filter.setAuthenticationSuccessHandler(new CustomLoginSuccessHandler());
-        return filter;
-    }
-
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
-                .httpBasic(AbstractHttpConfigurer::disable)
-//                .formLogin(Customizer.withDefaults())
-                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(auth -> auth.disable())
+                .formLogin(auth -> auth.disable())
+                .csrf(auth -> auth.disable())
+                .cors(cors -> this.corsConfigurationSource())
                 .authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/members").authenticated()
-                        .anyRequest().permitAll())
-                        .csrf(csrf -> csrf.disable())
-                        .cors(cors -> corsConfigurationSource());
-        http.addFilterAfter(this.authenticationFilter(this.authenticationManager(this.userDetailsService, this.passwordEncoder())), LogoutFilter.class);
+                        .requestMatchers("/register", "/login", "/").permitAll()
+                        .requestMatchers("/members").hasRole("ADMIN")
+                        .anyRequest().authenticated());
+
+        http.addFilterAt(
+                new JwtAuthenticationFilter(authenticationManager(authenticationConfiguration), jwtUtil),
+                UsernamePasswordAuthenticationFilter.class);
+
+        http.addFilterBefore(new JwtVerificationFilter(jwtUtil), JwtAuthenticationFilter.class);
+
+        // 세션 전략(Stateless) 설정
+        http
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
