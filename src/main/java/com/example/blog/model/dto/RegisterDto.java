d3bfd93aa7c71dcce1c3e848e472031e47ee18d1
highlaw00 <choiyool00@gmail.com>
2024-08-13 22:23:16 +0900

    refactor: 회원가입 DTO 분리

---
+++ src/main/java/com/example/blog/model/dto/RegisterDto.java
@@ -0,0 +1,19 @@
+package com.example.blog.model.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterDto {
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{6,20}$", message = "조건에 부합하지 않는 ID입니다.")
+    private String username;
+
+    // 최소 8글자 ~ 최대 30글자 & 영어, 숫자, 특수 기호 포함 가능.
+    @Pattern(regexp = "^[a-zA-Z0-9!@#$%^&*()\\-_=+\\[\\]{}|;:'\",.<>?/]{8,30}$", message = "조건에 부합하지 않는 비밀번호입니다.")
+    private String password;
+
+}
