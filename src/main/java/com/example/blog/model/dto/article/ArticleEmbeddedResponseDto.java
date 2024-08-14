7b3d720bf436810d13dbc87a6e52f286093e871c
highlaw00 <choiyool00@gmail.com>
2024-08-14 14:01:44 +0900

    refactor: 패키지 변경

---
+++ src/main/java/com/example/blog/model/dto/article/ArticleEmbeddedResponseDto.java
@@ -0,0 +1,24 @@
+package com.example.blog.model.dto.article;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ArticleEmbeddedResponseDto {
+    /**
+     * 임베딩 된 본문 내용에 포함될 내용
+     * ArticleID, ArticleCreatedAt, ArticleUpdatedAt, ArticleTitle
+     */
+    private Long id;
+    private String title;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public ArticleEmbeddedResponseDto(Long id, String title, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.title = title;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+}
