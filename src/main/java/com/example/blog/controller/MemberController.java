6b3b0dcf2fb8cb537b0580064828a710f265fd40
highlaw00 <choiyool00@gmail.com>
2024-08-14 13:57:32 +0900

    refactor: 변수명 통합 및 반환 타입 통합

--- src/main/java/com/example/blog/controller/MemberController.java
+++ src/main/java/com/example/blog/controller/MemberController.java
@@ -1,48 +1,38 @@
 package com.example.blog.controller;
 
-import com.example.blog.model.dto.ArticleResponseDto;
-import com.example.blog.model.dto.CommentDto;
-import com.example.blog.model.dto.CommentResponseDto;
-import com.example.blog.model.dto.MemberDto;
-import com.example.blog.exception.MemberNotFoundException;
-import com.example.blog.model.entity.Member;
-import com.example.blog.model.mapper.MemberMapper;
-import com.example.blog.repository.MemberRepository;
+import com.example.blog.model.dto.article.ArticleResponseDto;
+import com.example.blog.model.dto.comment.CommentResponseDto;
+import com.example.blog.model.dto.member.MemberResponseDto;
+import com.example.blog.model.dto.member.MemberUpdateRequestDto;
 import com.example.blog.service.ArticleService;
 import com.example.blog.service.CommentService;
 import com.example.blog.service.MemberService;
 import com.example.blog.utils.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/members")
 public class MemberController {
 
-    private final MemberRepository memberRepository;
     private final MemberService memberService;
     private final ArticleService articleService;
     private final CommentService commentService;
 
     @GetMapping
-    ApiResponse<List<MemberDto>> findMembers() {
-        List<Member> members = memberRepository.findAll();
-        List<MemberDto> dtos = MemberMapper.toDtos(members);
-        return ApiResponse.createSuccessResponse(dtos);
+    ApiResponse<List<MemberResponseDto>> findMembers() {
+        List<MemberResponseDto> responseDtos = memberService.findAll();
+        return ApiResponse.createSuccessResponse(responseDtos);
     }
 
     @GetMapping("/{id}")
-    ApiResponse<MemberDto> findMemberById(@PathVariable("id") Long id) {
-        MemberDto dto = memberService.findByIdAsDto(id);
-        return ApiResponse.createSuccessResponse(dto);
+    ApiResponse<MemberResponseDto> findMemberById(@PathVariable(name = "id") Long id) {
+        MemberResponseDto responseDto = memberService.findByIdAsDto(id);
+        return ApiResponse.createSuccessResponse(responseDto);
     }
 
     /*
@@ -51,10 +41,8 @@ public class MemberController {
         - Ex. 비밀번호를 제외한 필드를 수정
      */
     @PatchMapping("/{id}")
-    ApiResponse<MemberDto> updateMemberInfo(
-            @PathVariable("id") Long id, @Valid @RequestBody MemberDto memberDto
-    ) {
-        MemberDto responseDto = memberService.update(id, memberDto);
+    ApiResponse<MemberResponseDto> updateMemberInfo(@PathVariable(name = "id") Long id, @Valid @RequestBody MemberUpdateRequestDto memberDto) {
+        MemberResponseDto responseDto = memberService.update(id, memberDto);
         return ApiResponse.createSuccessResponse(responseDto);
     }
 
@@ -62,20 +50,20 @@ public class MemberController {
         TODO: 자격증명하여 인가된 사용자만 호출할 수 있도록 해야함
      */
     @DeleteMapping("/{id}")
-    ApiResponse<MemberDto> deleteMember(@PathVariable("id") Long id) {
-        MemberDto dto = memberService.delete(id);
-        return ApiResponse.createSuccessResponse(dto);
+    ApiResponse<MemberResponseDto> deleteMember(@PathVariable(name = "id") Long id) {
+        MemberResponseDto responseDto = memberService.delete(id);
+        return ApiResponse.createSuccessResponse(responseDto);
     }
 
     @GetMapping("/{id}/articles")
     public ApiResponse<List<ArticleResponseDto>> findArticlesOnMember(@PathVariable(name = "id") Long id) {
-        List<ArticleResponseDto> result = articleService.findAll(id);
-        return ApiResponse.createSuccessResponse(result);
+        List<ArticleResponseDto> responseDtos = articleService.findAll(id);
+        return ApiResponse.createSuccessResponse(responseDtos);
     }
 
     @GetMapping("/{id}/comments")
     public ApiResponse<List<CommentResponseDto>> findCommentsOnMember(@PathVariable(name = "id") Long id) {
-        List<CommentResponseDto> comments = commentService.findAllOnMember(id);
-        return ApiResponse.createSuccessResponse(comments);
+        List<CommentResponseDto> responseDtos = commentService.findAllOnMember(id);
+        return ApiResponse.createSuccessResponse(responseDtos);
     }
 }
